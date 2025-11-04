e0574216ace2685d8ec795fdd394c495d148a97d
nerdylua <nihaalsprakash.is23@rvce.edu.in>
2025-11-05 03:53:57 +0530

    added grid scan

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,32 @@
 import Shuffle from "@/components/Shuffle";
-import LiquidEther from "@/components/LiquidEther";
+import { GridScan } from "@/components/GridScan";
 
 export default function Home() {
   return (
-    <div className="relative flex min-h-screen items-center justify-center bg-white font-sans dark:bg-black">
-      <div className="absolute inset-0 z-0 pointer-events-auto">
-        <LiquidEther
-          className="w-full h-full"
-          colors={["#5227FF", "#FF9FFC", "#B19EEF"]}
-          mouseForce={18}
-          cursorSize={100}
-          isViscous={false}
-          viscous={20}
-          iterationsViscous={30}
-          iterationsPoisson={30}
-          resolution={0.4}
-          isBounce={false}
-          autoDemo={true}
-          autoSpeed={0.4}
-          autoIntensity={2.2}
-          takeoverDuration={0.25}
-          autoResumeDelay={2000}
-          autoRampDuration={0.4}
+    <div className="relative min-h-screen w-full bg-black font-sans">
+      <div className="absolute inset-0">
+        <GridScan
+          sensitivity={0.55}
+          lineThickness={1}
+          linesColor="#392e4e"
+          gridScale={0.1}
+          scanColor="#FF9FFC"
+          scanOpacity={0.4}
+          enablePost
+          bloomIntensity={0.6}
+          chromaticAberration={0.002}
+          noiseIntensity={0.01}
         />
       </div>
-      <main className="relative z-10 flex min-h-screen w-full max-w-3xl flex-col items-center justify-center p-8 pointer-events-none">
+      <main className="relative z-10 flex min-h-screen w-full max-w-3xl flex-col items-center justify-center p-8 mx-auto">
         <div className="w-fit">
           <Shuffle
             text="Testing cool ui components"
             tag="h1"
             textAlign="left"
-            className="normal-case whitespace-nowrap text-5xl sm:text-6xl font-semibold tracking-tight text-black dark:text-zinc-50"
+            className="normal-case whitespace-nowrap text-5xl sm:text-6xl font-semibold tracking-tight text-white"
           />
-          <p className="mt-2 w-full text-right text-xl sm:text-2xl text-zinc-700 dark:text-zinc-300">~ nerdylua</p>
+          <p className="mt-2 w-full text-right text-xl sm:text-2xl text-white">~ nerdylua</p>
         </div>
       </main>
     </div>
